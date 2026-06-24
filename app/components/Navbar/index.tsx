ceedfd4d8b78a128dc4b6cfa431bcc223b8769d1
Shreeyash Shrestha <shreeyash.shrestha@togglecorp.com>
2026-06-24 14:54:46 +0545

    feat(navbar): add logout dropdown in navbar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,43 +1,100 @@
 import {
+    use,
+    useCallback,
+} from 'react';
+import {
+    Button,
+    DropdownMenu,
     Heading,
     Image,
+    InlineLayout,
     ListView,
-    PageContainer,
 } from '@ifrc-go/ui';
+import { gql } from 'urql';
 
 import Link from '#components/Link';
+import UserContext from '#contexts/UserContext';
+import { useLogoutMutation } from '#generated/types/graphql';
+import useAlert from '#hooks/useAlert';
+import useRouting from '#hooks/useRouting';
 import Logo from '#resources/image/logo.png';
 
 import styles from './styles.module.css';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const LOGOUT = gql`
+    mutation Logout {
+        logout
+    }
+`;
+
 function Navbar() {
+    const { user, setUser } = use(UserContext);
+    const alert = useAlert();
+    const navigate = useRouting();
+
+    const [{ fetching: pendingLogout }, triggerLogout] = useLogoutMutation();
+
+    const handleLogout = useCallback(async () => {
+        const res = await triggerLogout({});
+        const logoutResponse = res.data?.logout;
+        if (logoutResponse) {
+            setUser(undefined);
+            navigate('login');
+            alert.show('Logout Successful', { variant: 'success' });
+        }
+    }, [navigate, triggerLogout, setUser, alert]);
+
     return (
         <nav className={styles.navbar}>
-            <PageContainer
-                className={styles.top}
-                contentClassName={styles.topContent}
-            >
-                <ListView
-                    withSpaceBetweenContents
-                >
-                    <Link
-                        to="home"
+            <InlineLayout
+                withPadding
+                withAdditionalInlinePadding
+                spacingOffset={1}
+                before={(
+                    <ListView
+                        withSpaceBetweenContents
                     >
-                        <ListView spacing="sm">
-                            <Image
-                                src={Logo}
-                                className={styles.icon}
-                                withoutBackground
-                            />
-                            <Heading
-                                level={4}
-                            >
-                                ERCS EOC
+                        <Link
+                            to="home"
+                        >
+                            <ListView spacing="sm">
+                                <Image
+                                    src={Logo}
+                                    className={styles.icon}
+                                    withoutBackground
+                                />
+                                <Heading
+                                    level={4}
+                                >
+                                    ERCS EOC
+                                </Heading>
+                            </ListView>
+                        </Link>
+                    </ListView>
+                )}
+                after={(
+                    <DropdownMenu
+                        labelStyleVariant="action"
+                        labelColorVariant="secondary"
+                        label={(
+                            <Heading level={6}>
+                                {user?.fullName}
                             </Heading>
-                        </ListView>
-                    </Link>
-                </ListView>
-            </PageContainer>
+                        )}
+                    >
+                        <Button
+                            name="logout"
+                            styleVariant="transparent"
+                            onClick={handleLogout}
+                            disabled={pendingLogout}
+                            withFullWidth
+                        >
+                            {pendingLogout ? 'Logging out' : 'Logout'}
+                        </Button>
+                    </DropdownMenu>
+                )}
+            />
         </nav>
     );
 }
